0306f6bf22f0f6f3edb7baee5871d9d9e63dde5d
bankymono <bankymono@gmail.com>
2021-09-17 12:31:00 +0100

    made updates to factor in buttons in the sub nav bar

--- frontend/src/components/SubNav/SubNav.js
+++ frontend/src/components/SubNav/SubNav.js
@@ -4,20 +4,24 @@ import {NavLink} from 'react-router-dom';
 
 import './SubNav.css';
 
-const SubNav = ({currentPage, arrLinks}) => {
+const SubNav = ({currentPage, arrLinks, rightButton}) => {
     return (
         <div className="subnav-container">
             <div className="subnav-heading">{currentPage}</div>
-            <ul className="nav-breadcrumbs"> 
-                {arrLinks.map((el,index,array)=>(
-                    <li key={index} className="nav-breadcrumbs-item">
-                        <NavLink 
-                            onClick={(event)=>{if(array.length - 1 === index) event.preventDefault()} } 
-                            to={`/${el}`} 
-                            className="nav-breadcrumb-link">{el}
-                        </NavLink>
-                    </li>))}
-            </ul>
+            <div className="subnav-wrapper">
+                <ul className="nav-breadcrumbs"> 
+                    {arrLinks.map((el,index,array)=>(
+                        <li key={index} className="nav-breadcrumbs-item">
+                            <NavLink 
+                                onClick={(event)=>{if(array.length - 1 === index) event.preventDefault()} } 
+                                to={`/${el}`} 
+                                className="nav-breadcrumb-link">{el}
+                            </NavLink>
+                        </li>))}
+                </ul>
+                {rightButton}
+            </div>
+
         </div>
     )
 }
