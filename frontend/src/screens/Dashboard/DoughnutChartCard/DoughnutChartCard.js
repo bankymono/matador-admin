12c231050f7cc51f545886ea5cf761179cccda94
Ayoola Taiwo <tayoola13@yahoo.com>
2021-11-27 14:31:32 +0100

    Reduced the number of breaks to four in the doughnut chart.

--- frontend/src/screens/Dashboard/DoughnutChartCard/DoughnutChartCard.js
+++ frontend/src/screens/Dashboard/DoughnutChartCard/DoughnutChartCard.js
@@ -7,10 +7,11 @@ const DoughnutChartCard = ({assetAllocationData}) => {
     const data =  {
         labels:false,
         datasets:[{
-            label:"Total Deposits('000)",
-            data:[3,3, 2, 3, 5],
-            borderColor: ['#FB9A99','rgba(31, 120, 180, 1)','rgba(166, 206, 227, 1)', '#B2DF8A','#33A02C'],
-            backgroundColor: ['#FB9A99','rgba(31, 120, 180, 1)','rgba(166, 206, 227, 1)', '#B2DF8A','#33A02C'],
+            label:"",
+            // data:[assetAllocationData.fixed_income.value, assetAllocationData.equity.value, assetAllocationData.cash.value, assetAllocationData.rewards.value],
+            data: [2,6,3,8],
+            borderColor: ['rgba(31, 120, 180, 1)','rgba(166, 206, 227, 1)', '#B2DF8A','#33A02C'],
+            backgroundColor: ['rgba(31, 120, 180, 1)','rgba(166, 206, 227, 1)', '#B2DF8A','#33A02C'],
             pointBackgroundColor: 'rgba(200, 189, 32, 0.2)',
             pointBorderColor: 'rgba(200, 189, 132, 0.2)',
         }],
@@ -24,7 +25,7 @@ const DoughnutChartCard = ({assetAllocationData}) => {
             <div className="dashboard-top-item dougnut-chart-card">
                 <div className="doughnut-card-heading-text">Asset Allocation</div>
                 <div className="doughnut-chart">
-                   <Doughnut  data={assetAllocationData} redraw={false} data={data} options={options} />
+                   <Doughnut redraw={false} data={data} options={options} />
                 </div>
                 <DoughnutChartTable assetAllocationData={assetAllocationData} />
             </div>
