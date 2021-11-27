cdef812088742d4c5100df4dc4ed8f37bc02e6f1
Ayoola Taiwo <tayoola13@yahoo.com>
2021-11-27 21:37:25 +0100

    All charts on the dashboard now responsive

--- frontend/src/components/Dashboard/DashboardInvestmentsCard/DashboardInvestmentsCard.js
+++ frontend/src/components/Dashboard/DashboardInvestmentsCard/DashboardInvestmentsCard.js
@@ -3,7 +3,7 @@ import { Bar } from 'react-chartjs-2';
 import './DashboardInvestmentsCard.css';
 
 const DashboardInvestmentsCard = () => {
-    const allTime = () => {
+    const allTimeForFirstChart = () => {
         return {
             labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
             datasets: [{
@@ -16,35 +16,67 @@ const DashboardInvestmentsCard = () => {
             }]
         }
     };
+    const allTimeForSecondChart = () => {
+        return {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            datasets: [{
+                label: "Total Deposits(000)",
+                data: [3, 0, 3, 5, 1, 3, 0, 1, 6, 12, 1, 12],
+                borderColor: 'rgba(73, 163, 123, 1)',
+                backgroundColor: 'rgba(73, 163, 123, 1)',
+                pointBackgroundColor: 'rgba(200, 189, 32, 0.2)',
+                pointBorderColor: 'rgba(200, 189, 132, 0.2)',
+            }]
+        }
+    };
+
     const [redrawFirstChart, setRedrawFirstChart] = useState(false);
-    const [firstChartData, setFirstChartData] = useState(allTime());
+    const [redrawSecondChart, setRedrawSecondChart] = useState(false);
+    const [firstChartData, setFirstChartData] = useState(allTimeForFirstChart());
+    const [secondChartData, setSecondChartData] = useState(allTimeForSecondChart());
     const setOptionsForFirstChart = (option) => {
         let selectedOption = option.target.value;
         setRedrawFirstChart(false);
+        let newData={...firstChartData};
         switch (selectedOption) {
-            case 'allTime':
-                setFirstChartData(allTime());
+            case 'allTimeForFirstChart':
+                setFirstChartData(allTimeForFirstChart());
                 break;
             case 'optionTwo':
-                setFirstChartData({
-                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-                    datasets: [{
-                        label: "Total Deposits(000)",
-                        data: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-                        borderColor: 'rgba(73, 163, 123, 1)',
-                        backgroundColor: 'rgba(73, 163, 123, 1)',
-                        pointBackgroundColor: 'rgba(200, 189, 32, 0.2)',
-                        pointBorderColor: 'rgba(200, 189, 132, 0.2)',
-                    }]
-                });
-                setRedrawFirstChart(!redrawFirstChart);
+                newData.datasets[0]['data'] = [2, 1, 0, 22, 3, 44, 55, 2, 11, 2, 3, 9];
+                setFirstChartData(newData);
+                break;
+            case 'optionThree':
+                newData.datasets[0]['data'] = [24.0, 1, 30, 22, 3, 4, 5, 2, 11, 2, 33, 9];
+                setFirstChartData(newData);
                 break;
             default:
-                setFirstChartData(allTime());
+                setFirstChartData(allTimeForFirstChart());
                 break;
         }
-        console.log(firstChartData);
         return setRedrawFirstChart(true);
+    }
+    const setOptionsForSecondChart = (option) => {
+        let selectedOption = option.target.value;
+        setRedrawSecondChart(false);
+        let newData={...secondChartData};
+        switch (selectedOption) {
+            case 'allTimeForSecondChart':
+                setSecondChartData(allTimeForSecondChart());
+                break;
+            case 'optionTwo':
+                newData.datasets[0]['data'] = [2, 1, 0, 22, 3, 44, 55, 2, 11, 2, 3, 9];
+                setSecondChartData(newData);
+                break;
+            case 'optionThree':
+                newData.datasets[0]['data'] = [24.0, 1, 30, 22, 3, 4, 5, 2, 11, 2, 33, 9];
+                setSecondChartData(newData);
+                break;
+            default:
+                setSecondChartData(allTimeForSecondChart());
+                break;
+        }
+        return setRedrawSecondChart(true);
     }
 
     return (
@@ -54,7 +86,7 @@ const DashboardInvestmentsCard = () => {
                 <div className="dashboard-investments-heading-container">
                     <div className="dashboard-investments-card-heading">Investments</div>
                     <select className="dashboard-select" onChange={setOptionsForFirstChart}>
-                        <option value='allTime'>All time</option>
+                        <option value='allTimeForFirstChart'>All time</option>
                         <option value='optionTwo'>option 2</option>
                         <option value='optionThree'>option 3</option>
                     </select>
@@ -72,8 +104,8 @@ const DashboardInvestmentsCard = () => {
             <div>
                 <div className="dashboard-investments-heading-container">
                     <div className="dashboard-investments-card-heading">Investments</div>
-                    <select className="dashboard-select" onChange={setOptionsForFirstChart}>
-                        <option value='allTime'>All time</option>
+                    <select className="dashboard-select" onChange={setOptionsForSecondChart}>
+                        <option value='allTimeForSecondChart'>All time</option>
                         <option value='optionTwo'>option 2</option>
                         <option value='optionThree'>option 3</option>
                     </select>
@@ -85,7 +117,7 @@ const DashboardInvestmentsCard = () => {
                 </div>
                 {/* <DashboardBarChartCard /> */}
                 <div className="dashboard-line-chart">
-                    <Bar height={150} redraw={redrawFirstChart} data={firstChartData} />
+                    <Bar height={150} redraw={redrawSecondChart} data={secondChartData} />
                 </div>
             </div>
 
