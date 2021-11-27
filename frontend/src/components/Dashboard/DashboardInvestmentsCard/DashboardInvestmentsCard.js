86fbc8db5869a1cdb673b89f47d3ed265dc41c98
Ayoola Taiwo <tayoola13@yahoo.com>
2021-11-27 17:46:19 +0100

    Bar chart now responsive with static data

--- frontend/src/components/Dashboard/DashboardInvestmentsCard/DashboardInvestmentsCard.js
+++ frontend/src/components/Dashboard/DashboardInvestmentsCard/DashboardInvestmentsCard.js
@@ -1,18 +1,62 @@
-import React from 'react'
-import DashboardBarChartCard from '../DashboardBarChartCard/DashboardBarChartCard';
+import React, { useState } from 'react'
+import { Bar } from 'react-chartjs-2';
 import './DashboardInvestmentsCard.css';
 
 const DashboardInvestmentsCard = () => {
+    const allTime = () => {
+        return {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            datasets: [{
+                label: "Total Deposits(000)",
+                data: [3, 2, 3, 5, 1, 3, 5, 1, 6, 10, 11, 12],
+                borderColor: 'rgba(73, 163, 123, 1)',
+                backgroundColor: 'rgba(73, 163, 123, 1)',
+                pointBackgroundColor: 'rgba(200, 189, 32, 0.2)',
+                pointBorderColor: 'rgba(200, 189, 132, 0.2)',
+            }]
+        }
+    };
+    const [redrawFirstChart, setRedrawFirstChart] = useState(false);
+    const [firstChartData, setFirstChartData] = useState(allTime());
+    const setOptionsForFirstChart = (option) => {
+        let selectedOption = option.target.value;
+        setRedrawFirstChart(false);
+        switch (selectedOption) {
+            case 'allTime':
+                setFirstChartData(allTime());
+                break;
+            case 'optionTwo':
+                setFirstChartData({
+                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                    datasets: [{
+                        label: "Total Deposits(000)",
+                        data: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+                        borderColor: 'rgba(73, 163, 123, 1)',
+                        backgroundColor: 'rgba(73, 163, 123, 1)',
+                        pointBackgroundColor: 'rgba(200, 189, 32, 0.2)',
+                        pointBorderColor: 'rgba(200, 189, 132, 0.2)',
+                    }]
+                });
+                setRedrawFirstChart(!redrawFirstChart);
+                break;
+            default:
+                setFirstChartData(allTime());
+                break;
+        }
+        console.log(firstChartData);
+        return setRedrawFirstChart(true);
+    }
+
     return (
         <div className="dashboard-top-item dashboard-investments-card">
-            
+
             <div>
                 <div className="dashboard-investments-heading-container">
                     <div className="dashboard-investments-card-heading">Investments</div>
-                    <select className="dashboard-select">
-                        <option>All time</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
+                    <select className="dashboard-select" onChange={setOptionsForFirstChart}>
+                        <option value='allTime'>All time</option>
+                        <option value='optionTwo'>option 2</option>
+                        <option value='optionThree'>option 3</option>
                     </select>
                 </div>
 
@@ -20,19 +64,29 @@ const DashboardInvestmentsCard = () => {
                     <div className="desc">Total Worth</div>
                     <div className="value">₦36,254,302<sup>.00</sup></div>
                 </div>
-                <DashboardBarChartCard />
+                <div className="dashboard-line-chart">
+                    <Bar height={150} redraw={redrawFirstChart} data={firstChartData} />
+                </div>
             </div>
 
             <div>
                 <div className="dashboard-investments-heading-container">
                     <div className="dashboard-investments-card-heading">Investments</div>
+                    <select className="dashboard-select" onChange={setOptionsForFirstChart}>
+                        <option value='allTime'>All time</option>
+                        <option value='optionTwo'>option 2</option>
+                        <option value='optionThree'>option 3</option>
+                    </select>
                 </div>
 
                 <div className="investments-value">
                     <div className="desc">Total Number</div>
                     <div className="value">₦42,254</div>
                 </div>
-                <DashboardBarChartCard />
+                {/* <DashboardBarChartCard /> */}
+                <div className="dashboard-line-chart">
+                    <Bar height={150} redraw={redrawFirstChart} data={firstChartData} />
+                </div>
             </div>
 
         </div>
--- frontend/src/screens/Dashboard/DoughnutChartCard/DoughnutChartCard.js
+++ frontend/src/screens/Dashboard/DoughnutChartCard/DoughnutChartCard.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './DoughnutChartCard.css'
 import {Doughnut} from 'react-chartjs-2';
 import DoughnutChartTable from './DoughnutChartTable/DoughnutChartTable';
 
-const DoughnutChartCard = ({assetAllocationData}) => { 
-    const data =  {
+const DoughnutChartCard = ({assetAllocationData}) => {
+    const [redraw, setRedraw] = useState(false); 
+    const [data, setData] = useState({
         labels:false,
         datasets:[{
             label:"",
@@ -15,7 +16,23 @@ const DoughnutChartCard = ({assetAllocationData}) => {
             pointBackgroundColor: 'rgba(200, 189, 32, 0.2)',
             pointBorderColor: 'rgba(200, 189, 132, 0.2)',
         }],
-    }
+    });
+    const resetData =()=>{
+        setData(({
+            labels:false,
+            datasets:[{
+                label:"",
+                // data:[assetAllocationData.fixed_income.value, assetAllocationData.equity.value, assetAllocationData.cash.value, assetAllocationData.rewards.value],
+                data: [5,5,5,5],
+                borderColor: ['rgba(31, 120, 180, 1)','rgba(166, 206, 227, 1)', '#B2DF8A','#33A02C'],
+                backgroundColor: ['rgba(31, 120, 180, 1)','rgba(166, 206, 227, 1)', '#B2DF8A','#33A02C'],
+                pointBackgroundColor: 'rgba(200, 189, 32, 0.2)',
+                pointBorderColor: 'rgba(200, 189, 132, 0.2)',
+            }],
+        }));
+        setRedraw(!redraw);
+        return data;
+    };
 
     var options = {        
         cutout: '90%',
@@ -25,9 +42,10 @@ const DoughnutChartCard = ({assetAllocationData}) => {
             <div className="dashboard-top-item dougnut-chart-card">
                 <div className="doughnut-card-heading-text">Asset Allocation</div>
                 <div className="doughnut-chart">
-                   <Doughnut redraw={false} data={data} options={options} />
+                   <Doughnut redraw={redraw} data={data} options={options} />
                 </div>
                 <DoughnutChartTable assetAllocationData={assetAllocationData} />
+                <div onClick={()=>resetData()}>redraw doughnut button</div>
             </div>
     )
 }
