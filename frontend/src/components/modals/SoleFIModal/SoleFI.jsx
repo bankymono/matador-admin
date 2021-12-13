6f2792b3b8c07c1d0bc2a1e3bb21d9522f872671
bankymono <bankymono@gmail.com>
2021-12-13 18:50:48 +0100

    complete implementation of ui of sole detail

--- frontend/src/components/modals/SoleFIModal/SoleFI.jsx
+++ frontend/src/components/modals/SoleFIModal/SoleFI.jsx
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import './SoleFI.css';
 
 import close_icon from '../../../assets/icons/close-icon.png';
+import profile_img from '../../../assets/images/profile-img.png';
 
 import { useLocation } from 'react-router-dom';
 
@@ -27,6 +28,7 @@ const SoleFI = ({setIsOpen, open, onClose}) => {
     return ReactDom.createPortal(
         <>
             <div onClick={onClose} className="s-f-i-container">
+                <div className='s-f-i-input-wrapper-main'>
                 <div onClick={(event)=> event.stopPropagation()} className="s-f-i-input-wrapper">
                     <div className="s-f-i-heading">
                         <div>Investment Info</div>
@@ -37,7 +39,7 @@ const SoleFI = ({setIsOpen, open, onClose}) => {
                        <div className='detail-card-one'>
                             <div className='heading-wrapper'>
                                 <div className='heading-item-one'>Details</div>
-                                <div className='heading-status-item'>Active</div>
+                                <div className='heading-status-item active'>Active</div>
                             </div>
 
                             <div className='body-wrapper'>
@@ -45,18 +47,145 @@ const SoleFI = ({setIsOpen, open, onClose}) => {
                                     <div className='body-item-desc'>Amount invested</div>
                                     <div className='body-item-value'>₦23,120,000.00</div>
                                 </div>
+
+                                <div className='body-item'>
+                                    <div className='body-item-desc'>Amount remaining</div>
+                                    <div className='body-item-value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='body-item'>
+                                    <div className='body-item-desc'>Duration</div>
+                                    <div className='body-item-value'>65 days</div>
+                                </div>
+
+                                <div className='body-item'>
+                                    <div className='body-item-desc'>Collection number</div>
+                                    <div className='body-item-value'>2</div>
+                                </div>
+
+                                <div className='body-item'>
+                                    <div className='body-item-desc'>Investors</div>
+                                    <div className='body-item-value'>7</div>
+                                </div>
+
+                                <div className='body-item'>
+                                    <div className='body-item-desc'>Creator</div>
+                                    <div className='body-item-value'>Abubakar Yanladı</div>
+                                </div>
+
+                                <div className='body-item'>
+                                    <div className='body-item-desc'>Collection code</div>
+                                    <div className='body-item-value'>11348t6</div>
+                                </div>
                             </div>
                        </div>
 
                        <div className='detail-card-two'>
                             <div className='card-two-heading'>Investors</div>
                             <div className='card-two-body-wrapper'>
+
                                 <div className='card-item'>
                                     <div className='info-container'>
-                                        <img src="" alt="" />
+                                        <img src={profile_img} alt="" />
                                         <div className='name-details'>
-                                            <div className='name'></div>
-                                            <div className='role'></div>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
+                                        </div>
+                                    </div>
+
+                                    <div className='value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='card-item'>
+                                    <div className='info-container'>
+                                        <img src={profile_img} alt="" />
+                                        <div className='name-details'>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
+                                        </div>
+                                    </div>
+
+                                    <div className='value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='card-item'>
+                                    <div className='info-container'>
+                                        <img src={profile_img} alt="" />
+                                        <div className='name-details'>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
+                                        </div>
+                                    </div>
+
+                                    <div className='value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='card-item'>
+                                    <div className='info-container'>
+                                        <img src={profile_img} alt="" />
+                                        <div className='name-details'>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
+                                        </div>
+                                    </div>
+
+                                    <div className='value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='card-item'>
+                                    <div className='info-container'>
+                                        <img src={profile_img} alt="" />
+                                        <div className='name-details'>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
+                                        </div>
+                                    </div>
+
+                                    <div className='value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='card-item'>
+                                    <div className='info-container'>
+                                        <img src={profile_img} alt="" />
+                                        <div className='name-details'>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
+                                        </div>
+                                    </div>
+
+                                    <div className='value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='card-item'>
+                                    <div className='info-container'>
+                                        <img src={profile_img} alt="" />
+                                        <div className='name-details'>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
+                                        </div>
+                                    </div>
+
+                                    <div className='value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='card-item'>
+                                    <div className='info-container'>
+                                        <img src={profile_img} alt="" />
+                                        <div className='name-details'>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
+                                        </div>
+                                    </div>
+
+                                    <div className='value'>₦23,120,000.00</div>
+                                </div>
+
+                                <div className='card-item'>
+                                    <div className='info-container'>
+                                        <img src={profile_img} alt="" />
+                                        <div className='name-details'>
+                                            <div className='name'>Abubakar Yanladı</div>
+                                            <div className='role'>Creator</div>
                                         </div>
                                     </div>
 
@@ -65,6 +194,7 @@ const SoleFI = ({setIsOpen, open, onClose}) => {
                             </div>
                        </div>
                     </div>
+                </div>
                 </div>
             </div>        
         </>,
