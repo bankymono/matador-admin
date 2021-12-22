a7a45312ef1a55207e576d1d32a5ee4c993d12b1
Ayoola Taiwo <tayoola13@yahoo.com>
2021-12-22 12:20:53 +0100

    Removed unused commented code from the stat card

--- frontend/src/components/Projects/ProjectsListOverviewCard/ProjectsListOverviewCard.js
+++ frontend/src/components/Projects/ProjectsListOverviewCard/ProjectsListOverviewCard.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import './ProjectsListOverviewCard.css';
 
-import right_arrow_icon from '../../../assets/icons/comma-right-arrow-icon.png'
-
 const ProjectListOverviewCard = ({number_of_projects}) => {
     return (
         <div className="projects-list-overview-card-container">
@@ -16,11 +14,6 @@ const ProjectListOverviewCard = ({number_of_projects}) => {
                         <div className="desc">Number of Projects</div>
                         <div className="value">{number_of_projects}</div>
                     </div>
-
-                    {/* <div className="projects-value-container">
-                        <div className="desc">Revenue Guaranteed</div>
-                        <div className="value">₦36,254</div>
-                    </div> */}
                 </div>
             </div>
         </div>
